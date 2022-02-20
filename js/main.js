f0ed4c55463682b07b8c780b108221931c0f7a5c
yeju95 <99623943+yeju95@users.noreply.github.com>
2022-02-20 18:26:47 +0900

    new

--- js/main.js
+++ js/main.js
@@ -63,34 +63,9 @@ $('.slides').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
 
 
 $('.move').slick({

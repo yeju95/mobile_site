15db6356ace81fc1f320c4373aac3b62c42373f0
yeju95 <99623943+yeju95@users.noreply.github.com>
2022-02-21 10:47:21 +0900

    new

--- js/main.js
+++ js/main.js
@@ -1,67 +1,84 @@
-var myFullpage = new fullpage('#fullpage', {
-    menu: '#menu',
-    anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
-    autoScrolling: true,  //스크롤 바를 스크롤 하면 한 페이지씩 넘어감(기본설정)
-    scrollBar:true
+$(function(){
+
+//햄버거 버튼을 클릭하면
+$('.open_gnb').click(function(){
+    $(this).toggleClass('open');
+    $('.gnb_area').slideToggle();
+    $('.m_gnb_dimmed').fadeToggle(200);
 });
 
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 3,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
+
+//main
+    var sliderUl = $('#slider>ul');          
+    var imgWidth = $('#slider img').width(); 
+    var imgNumber = $('#slider li').length;  
+    console.log(imgWidth);
+
+    sliderUl.css({width:imgWidth*imgNumber});   
+
+
+    var right = function(){
+        sliderUl.find('li:last-child').insertBefore(sliderUl.find('li:first-child')); 
+        sliderUl.css({'margin-left':-500});
+        sliderUl.animate({'margin-left':0},300);
+    };
+
+
+    var left = function(){
+        sliderUl.animate({'margin-left':500},300,'swing',function(){
+            sliderUl.css({'margin-left':0});
+            sliderUl.find("li:first-child").insertAfter(sliderUl.find("li:last-child"));
+        });    
+    }; 
+
+
+    $(".right").click(function(){
+        right();
+    });
+
+
+    $(".left").click(function(){
+        left();
+    });
+
+
+
+
+    var du = 400; 
+
+$(".first .item").on("mouseenter",function(){
+  $(this).find("strong, span").stop().animate({opacity:1},du);
+})
+.on("mouseleave",function(){
+  $(this).find("strong, span").stop().animate({opacity:""},du);
 });
 
-var swiper = new Swiper(".mySwiper_1", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 3,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2_1", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
+
+
+
+
+
+//슬라이더-slick
+$('.slides').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
 });
 
-var swiper = new Swiper(".mySwiper_2", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 3,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2_2", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
 
-function winOpen(){
-  window.open('https://yeju95.github.io/mobile_site/','popup','width=437, height=915,resizable=no ')
-}
+$('.move').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    fade: true,
+    cssEase: 'linear',
+    arrows:false,
+    autoplay: true,
+  });
+    
+
+
+
+});
